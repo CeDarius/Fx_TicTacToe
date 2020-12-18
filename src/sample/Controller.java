cdb47e650b8d95a69d3767d90a0d133ff74ba254
CeDarius <cekysdarius@gmail.com>
2020-12-18 18:02:35 +0200

    Update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -180,19 +180,11 @@ public class Controller {
                 (table[0]=='x' && table[3]=='x' && table[6]=='x') ||
                 (table[1]=='x' && table[4]=='x' && table[7]=='x') ||
                 (table[2]=='x' && table[5]=='x' && table[8]=='x') ||
-                        (table[0]=='x' && table[4]=='x' && table[8]=='x') ||
-                        (table[2]=='x' && table[4]=='x' && table[6]=='x')) {
+                (table[0]=='x' && table[4]=='x' && table[8]=='x') ||
+                (table[2]=='x' && table[4]=='x' && table[6]=='x')) {
             labelPlayer.setText("");
             labelWinner.setText("Winner Player One");
-            zeroIsFinished = true;
-            oneIsFinished = true;
-            twoIsFinished = true;
-            threeIsFinished = true;
-            fourIsFinished = true;
-            fiveIsFinished = true;
-            sixIsFinished = true;
-            sevenIsFinished = true;
-            eightIsFinished = true;
+            lockButtons();
         } else if ((table[0]=='o' && table[1]=='o' && table[2]=='o') ||
                 (table[3]=='o' && table[4]=='o' && table[5]=='o') ||
                 (table[6]=='o' && table[7]=='o' && table[8]=='o') ||
@@ -203,15 +195,7 @@ public class Controller {
                 (table[2]=='o' && table[4]=='o' && table[6]=='o')) {
             labelPlayer.setText("");
             labelWinner.setText("Winner Player Two");
-            zeroIsFinished = true;
-            oneIsFinished = true;
-            twoIsFinished = true;
-            threeIsFinished = true;
-            fourIsFinished = true;
-            fiveIsFinished = true;
-            sixIsFinished = true;
-            sevenIsFinished = true;
-            eightIsFinished = true;
+            lockButtons();
         }
         System.out.println("check over");
     }
@@ -248,6 +232,18 @@ public class Controller {
         }else {
             labelPlayer.setText(playerTwo);
         }
+    }
+
+    public void lockButtons() {
+        zeroIsFinished = true;
+        oneIsFinished = true;
+        twoIsFinished = true;
+        threeIsFinished = true;
+        fourIsFinished = true;
+        fiveIsFinished = true;
+        sixIsFinished = true;
+        sevenIsFinished = true;
+        eightIsFinished = true;
     }
 
 
